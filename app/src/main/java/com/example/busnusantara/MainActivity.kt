f91822f4bf5e002f50372252325d5ef8dd13f78b
Ezra Sitorus <es1419@ic.ac.uk>
2021-06-04 01:05:21 +0100

    Searches route through database

--- app/src/main/java/com/example/busnusantara/MainActivity.kt
+++ app/src/main/java/com/example/busnusantara/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.busnusantara
 
-import android.content.ContentValues.TAG
+import android.content.ContentValues
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+import com.example.busnusantara.database.*
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,78 +16,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        saveOrderToDB("1000", "1000", 19.95, "Jakarta")
-        saveRouteToDB("Yogyakarta", listOf<String>("Surabaya, Bali, Jakarta"))
-        saveAgentToDB("Jeff", "Jakarta")
+
+        btnSearchRoute.setOnClickListener {
+            val startLocation = etStartLocation.text.toString()
+            val destination = etDestination.text.toString()
+
+            if (startLocation.isNotEmpty() && destination.isNotEmpty()) {
+                Firebase.firestore.collection(Collections.ROUTES.toString())
+                    .whereEqualTo("destination", destination)
+                    .get().addOnSuccessListener { documents ->
+                        Log.d(ContentValues.TAG, "Finding all routes with $destination dest")
+                        if (documents.isEmpty) {
+                            textView.text = "No route was found. Please try again"
+                        }
+                        for (document in documents) {
+                            textView.text =
+                                "There is a route going through: " + document.data.get("stops")
+                                    .toString()
+                            Log.d(ContentValues.TAG, "${document.id} => ${document.data}")
+                        }
+                    }
+            }
+        }
     }
-
-    // Create a new Order
-    private fun saveOrderToDB(
-        routeID: String,
-        agentID: String,
-        price: Double,
-        pickupLocation: String
-    ) {
-
-        val order = hashMapOf(
-            "routeID" to routeID,
-            "agentId" to agentID,
-            "price" to price,
-            "pickupLocation" to pickupLocation
-        )
-
-        // Add a new document with a generated ID
-        Firebase.firestore.collection("Orders")
-            .add(order)
-            .addOnSuccessListener { documentReference ->
-                Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
-            }
-            .addOnFailureListener { e ->
-                Log.w(TAG, "Error adding document", e)
-            }
-    }
-
-    // Create a new Route
-    private fun saveRouteToDB(
-        destination: String, stops: List<String>
-    ) {
-
-        val route = hashMapOf(
-            "destination" to destination,
-            "stops" to stops,
-        )
-
-        // Add a new document with a generated ID
-        Firebase.firestore.collection("Routes")
-            .add(route)
-            .addOnSuccessListener { documentReference ->
-                Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
-            }
-            .addOnFailureListener { e ->
-                Log.w(TAG, "Error adding document", e)
-            }
-    }
-
-    // Create a new Agent
-    private fun saveAgentToDB(
-        name: String, locationBased: String
-    ) {
-
-        val order = hashMapOf(
-            "destination" to name,
-            "stops" to locationBased,
-        )
-
-        // Add a new document with a generated ID
-        Firebase.firestore.collection("Agents")
-            .add(order)
-            .addOnSuccessListener { documentReference ->
-                Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
-            }
-            .addOnFailureListener { e ->
-                Log.w(TAG, "Error adding document", e)
-            }
-    }
-
-
 }
