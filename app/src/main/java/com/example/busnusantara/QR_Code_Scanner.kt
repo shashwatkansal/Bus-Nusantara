288996fe4a935d25e3ac148df6f2edd0ca53017a
Ezra Sitorus <es1419@ic.ac.uk>
2021-06-07 19:58:43 +0100

    Implemented functionality of scanner

--- app/src/main/java/com/example/busnusantara/QR_Code_Scanner.kt
+++ app/src/main/java/com/example/busnusantara/QR_Code_Scanner.kt
@@ -1,14 +1,20 @@
 package com.example.busnusantara
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.budiyev.android.codescanner.AutoFocusMode
 import com.budiyev.android.codescanner.CodeScanner
 import com.budiyev.android.codescanner.DecodeCallback
 import com.budiyev.android.codescanner.ErrorCallback
 import com.budiyev.android.codescanner.ScanMode
 import kotlinx.android.synthetic.main.activity_qr_code_scanner.*
+
+private const val CAMERA_REQUEST_CODE = 101
 
 class QR_Code_Scanner : AppCompatActivity() {
 
@@ -17,6 +23,9 @@ class QR_Code_Scanner : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_qr_code_scanner)
+
+        setUpPermissions()
+        codeScanner()
     }
 
     private fun codeScanner() {
@@ -43,7 +52,49 @@ class QR_Code_Scanner : AppCompatActivity() {
                 }
             }
         }
+    }
 
+    override fun onResume() {
+        super.onResume()
+        codeScanner.startPreview()
+    }
 
+    override fun onPause() {
+        codeScanner.releaseResources()
+        super.onPause()
+    }
+
+    private fun setUpPermissions() {
+        val permission = ContextCompat.checkSelfPermission(
+            this,
+            android.Manifest.permission.CAMERA
+        )
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            makeRequest()
+        }
+    }
+
+    private fun makeRequest() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(android.Manifest.permission.CAMERA),
+            CAMERA_REQUEST_CODE
+        )
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            CAMERA_REQUEST_CODE -> {
+                if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "You need the camera permission", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
+        }
     }
 }
