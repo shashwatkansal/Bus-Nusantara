cef7f35b5d6391e44a050d2fe59e09e67a1256d5
Ezra Sitorus <es1419@ic.ac.uk>
2021-06-07 17:58:50 +0100

    Created QR Code Scanner Activity

---
+++ app/src/main/java/com/example/busnusantara/QR_Code_Scanner.kt
@@ -0,0 +1,49 @@
+package com.example.busnusantara
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.budiyev.android.codescanner.AutoFocusMode
+import com.budiyev.android.codescanner.CodeScanner
+import com.budiyev.android.codescanner.DecodeCallback
+import com.budiyev.android.codescanner.ErrorCallback
+import com.budiyev.android.codescanner.ScanMode
+import kotlinx.android.synthetic.main.activity_qr_code_scanner.*
+
+class QR_Code_Scanner : AppCompatActivity() {
+
+    private lateinit var codeScanner: CodeScanner
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_qr_code_scanner)
+    }
+
+    private fun codeScanner() {
+        codeScanner = CodeScanner(this, scanner_view)
+
+        codeScanner.apply {
+            camera = CodeScanner.CAMERA_BACK
+            formats = CodeScanner.ALL_FORMATS
+
+            autoFocusMode = AutoFocusMode.SAFE
+            scanMode = ScanMode.CONTINUOUS
+            isAutoFocusEnabled = true
+            isFlashEnabled = false
+
+            decodeCallback = DecodeCallback {
+                runOnUiThread {
+                    tvQRScan.text = it.text
+                }
+            }
+
+            errorCallback = ErrorCallback {
+                runOnUiThread {
+                    Log.e("Main", "Camera initialization error: ${it.message}")
+                }
+            }
+        }
+
+
+    }
+}
