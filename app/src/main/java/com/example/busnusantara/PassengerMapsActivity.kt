6a3b5839822f9abe08dd4ab5bbfc79f630745070
shashwatkansal <sk4319@ic.ac.uk>
2021-06-10 13:16:46 +0100

    Add markers for passenger, bus driver, stops and build route

--- app/src/main/java/com/example/busnusantara/PassengerMapsActivity.kt
+++ app/src/main/java/com/example/busnusantara/PassengerMapsActivity.kt
@@ -1,18 +1,20 @@
 package com.example.busnusantara
 
 import android.content.ContentValues
+import android.content.ContentValues.TAG
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.busnusantara.database.Collections
 import com.example.busnusantara.databinding.ActivityPassengerMapsBinding
-import com.example.busnusantara.googleapi.GetPathFromLocation
+import com.example.busnusantara.googleapi.buildRoute
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.GeoPoint
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
@@ -22,7 +24,6 @@ class PassengerMapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityPassengerMapsBinding
-    private lateinit var driverLocation: GeoPoint
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,72 +43,111 @@ class PassengerMapsActivity : AppCompatActivity(), OnMapReadyCallback {
      */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
-        val routeID = "IT5cRoAVJjl16gy0bfGh"
-        val startBusStop = LatLng(-6.224854, 106.827)
+        val orderID = "oolO6KVivO3Z445xu5cW"
 
-        getDriverLocation(routeID)
-        mMap.addMarker(
-            MarkerOptions().position(startBusStop)
-                .title("Your nearest bus stop")
-        )
+        getPassengerBusStopAndMark(orderID)
+        mMap.setMinZoomPreference(6.0f)
+        mMap.setMaxZoomPreference(14.0f)
+
     }
 
-    private fun getDriverLocation(routeID: String) {
-        /* TODO: Implement */
-        val collection = Collections.ROUTES.toString()
-        Firebase.firestore.collection(collection)
-            .get()
-            .addOnSuccessListener { routes ->
-                Log.d(ContentValues.TAG, "Finding all $collection with routeID $routeID")
-                if (routes.isEmpty || routes.size() > 1) {
-                    Log.d(
-                        ContentValues.TAG,
-                        "routeID $routeID doesn't exist or there are too many such IDs."
-                    )
-                }
+    private fun getPassengerBusStopAndMark(orderId: String) {
+        Firebase.firestore.collection(Collections.ORDERS.toString())
+            .document(orderId).get().addOnSuccessListener { order ->
+                if (order != null) {
+                    val pickupLocation = order["pickupLocation"] as String
+                    val tripDocRef = order["tripID"] as DocumentReference
+                    val tripRef = tripDocRef.id.toString()
+                    Log.d(TAG, "tripRef is $tripRef")
+                    Log.d(TAG, "pickupLocation is $pickupLocation")
 
-                // Only one routeID, get DocumentReference to find the driver.
-                for (route in routes) {
-                    Firebase.firestore.collection(Collections.TRIPS.toString())
-                        .whereEqualTo("routeID", route.reference)
-                        .get().addOnSuccessListener { driversTrips ->
-                            if (driversTrips.isEmpty) {
-                                Log.d(
-                                    ContentValues.TAG,
-                                    "routeID $routeID doesn't have a driver!."
-                                )
-                            } else {
-                                for (driverTrip in driversTrips) {
-                                    driverLocation = driverTrip.data["location"] as GeoPoint
-                                    Log.d(
-                                        ContentValues.TAG,
-                                        "Updating map to new location of driver: $driverLocation."
-                                    )
-                                    val driverLoc =
-                                        LatLng(driverLocation.latitude, driverLocation.longitude)
-
-                                    // Add a marker for Bus Driver's Location and move the camera
+                    // Mark the passenger's location on map
+                    Firebase.firestore.collection(Collections.AGENTS.toString())
+                        .whereEqualTo("locationBased", pickupLocation)
+                        .get().addOnSuccessListener { agents ->
+                            if (!agents.isEmpty) {
+                                for (agent in agents) {
+                                    Log.d(TAG, "Getting Agent $agent")
+                                    val passengerLoc = agent?.getGeoPoint("coordinate")?.let {
+                                        geoPointToLatLng(
+                                            it
+                                        )
+                                    }
                                     mMap.addMarker(
-                                        MarkerOptions().position(driverLoc)
-                                            .title("Bus Driver's Location")
+                                        MarkerOptions().position(passengerLoc)
+                                            .title("Your stop")
                                     )
-                                    mMap.moveCamera(CameraUpdateFactory.newLatLng(driverLoc))
-
-                                    GetPathFromLocation(
-                                        driverLoc, LatLng(-6.224854, 106.827)
-                                    ) { polyLine -> mMap.addPolyline(polyLine) }.execute()
-
+                                    mMap.moveCamera(CameraUpdateFactory.newLatLng(passengerLoc))
                                 }
                             }
                         }
 
+                    /* Get the driver's location and mark it */
+                    Firebase.firestore.collection(Collections.TRIPS.toString())
+                        .document(tripRef).get().addOnSuccessListener { trip ->
+                            Log.d(TAG, "Getting trip $trip")
+                            if (trip != null) {
+                                val incomingDriverLocation = trip.data?.get("location") as GeoPoint
 
+                                mMap.addMarker(
+                                    MarkerOptions()
+                                        .position(geoPointToLatLng(incomingDriverLocation))
+                                        .title("Bus Driver")
+                                )
+
+                                // Mark all the stops on map
+                                val routeDocRef = trip.data!!["routeID"] as DocumentReference
+                                val routeID = routeDocRef.id
+                                Firebase.firestore.collection(Collections.ROUTES.toString())
+                                    .document(routeID).get().addOnSuccessListener { route ->
+                                        Log.d(TAG, "Getting route $route")
+                                        if (route != null) {
+                                            var start = route.get("start") as String
+                                            Log.d(TAG, "Found start location $start")
+                                            val stops = route.get("stops") as ArrayList<String>
+                                            Log.d(TAG, "Found stops locations $stops")
+                                            addStopOnMap(start)
+
+                                            for (stop in stops) {
+                                                Log.d(TAG, "Adding stop $stop on map")
+                                                addStopOnMap(stop)
+                                                buildRoute(start, stop, mMap)
+                                                start = stop
+                                            }
+                                        }
+                                    }
+                            }
+                        }
                 }
             }
     }
 
     private fun getBusETA() {
         /* TODO: Implement */
+    }
+
+    private fun addStopOnMap(stop: String) {
+        Firebase.firestore.collection(Collections.AGENTS.toString())
+            .whereEqualTo("locationBased", stop)
+            .get().addOnSuccessListener { documents ->
+                Log.d(ContentValues.TAG, "Finding stop $stop agent")
+                if (!documents.isEmpty) {
+                    for (document in documents) {
+                        val coordinate: GeoPoint? = document.getGeoPoint("coordinate")
+                        if (coordinate != null) {
+                            val lat = coordinate.latitude
+                            val lng = coordinate.longitude
+                            val agent = LatLng(lat, lng)
+
+                            mMap.addMarker(MarkerOptions().position(agent).title(stop))
+                        }
+                    }
+                }
+            }
+    }
+
+    private fun geoPointToLatLng(geoPoint: GeoPoint): LatLng {
+        return LatLng(geoPoint.latitude, geoPoint.longitude)
     }
 
 
