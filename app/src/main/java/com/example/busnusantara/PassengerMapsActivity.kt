5faca2031d200a03b6bc436fe9cc3938b1f69aaf
shashwatkansal <sk4319@ic.ac.uk>
2021-06-10 19:15:33 +0100

    Change bus marker and passenger bus stop marker colours, and fix route with bus location to next stop

--- app/src/main/java/com/example/busnusantara/PassengerMapsActivity.kt
+++ app/src/main/java/com/example/busnusantara/PassengerMapsActivity.kt
@@ -12,6 +12,7 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.firebase.firestore.DocumentReference
@@ -75,7 +76,11 @@ class PassengerMapsActivity : AppCompatActivity(), OnMapReadyCallback {
                                     }
                                     mMap.addMarker(
                                         MarkerOptions().position(passengerLoc)
-                                            .title("Your stop")
+                                            .title("Your stop").icon(
+                                                BitmapDescriptorFactory.defaultMarker(
+                                                    BitmapDescriptorFactory.HUE_ORANGE
+                                                )
+                                            )
                                     )
                                     mMap.moveCamera(CameraUpdateFactory.newLatLng(passengerLoc))
                                 }
@@ -91,6 +96,11 @@ class PassengerMapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
                                 mMap.addMarker(
                                     MarkerOptions()
+                                        .icon(
+                                            BitmapDescriptorFactory.defaultMarker(
+                                                BitmapDescriptorFactory.HUE_ORANGE
+                                            )
+                                        )
                                         .position(geoPointToLatLng(incomingDriverLocation))
                                         .title("Bus Driver")
                                 )
@@ -114,6 +124,11 @@ class PassengerMapsActivity : AppCompatActivity(), OnMapReadyCallback {
                                                 buildRoute(start, stop, mMap)
                                                 start = stop
                                             }
+                                            buildRoute(
+                                                geoPointToLatLng(incomingDriverLocation),
+                                                start,
+                                                mMap
+                                            )
                                         }
                                     }
                             }
